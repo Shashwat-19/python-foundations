c62d69fb22f6673ee2405a90e21bc15716267c36
Shashwat.! <shashwat1956@gmail.com>
2026-04-11 16:39:25 +0530

    New Question added.

--- Module-02/quest/quest1.py
+++ Module-02/quest/quest1.py
@@ -1,12 +1,7 @@
 #  write a program to check if the number which is entered by the user is odd or even.abs
 
 num = int(input("Enter the number: "))
-
-if (num % 2 == 0):
-    print("The number is Even.")
-elif(num % 2 != 0):
-    print("The number is odd.")
+if (num%2 == 0):
+    print(f"The number {num} is even.")
 else:
-    print("Invalid Number.")
-
-print("="* 29)
+    print(f"The number {num} is odd.")
--- Module-02/quest/quest2.py
+++ Module-02/quest/quest2.py
@@ -1,13 +1,12 @@
 # Write a program for checking which is the greatest number among three.
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-num3 = int(input("Enter third number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
 
 if (num1 > num2 and num1 > num3):
-    print(f"{num1} is greater than {num2} and {num3}")
-elif(num2 > num1 and num2>num3):
-    print(f"{num2} is greater than {num1} and {num3}")
+    print(f"The number {num1} is greatest number of all.")
+elif(num2 > num1 and num2 > num3):
+    print(f"The number {num2} is greatest number of all.")
 else:
-    print(f"{num3} is greater than {num1} and {num2}")
-print("*"*29)
+    print(f"The number {num3} is greatest number of all.")
