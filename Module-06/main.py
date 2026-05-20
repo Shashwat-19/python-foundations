0ff26bbfe4d8a50fc3dd143b1b470055dde024ca
Shashwat.! <Shashwat-19@users.noreply.github.com>
2026-05-20 16:58:15 +0530

    Added: New module.

---
+++ Module-06/main.py
@@ -0,0 +1,14 @@
+#  functions
+
+# earlier we were writing code like this :
+a = 10
+b = 23
+sum = a+b
+print(sum)
+
+
+# but now we will use
+def calculate_sum(a,b):
+    sum = a+b
+    print(sum)
+calculate_sum(4,6)
