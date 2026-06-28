2696f37ce749de6b7a8e3ba947724a7d626b0e25
Shashwat <shashwat1956@gmail.com>
2026-06-29 04:57:17 +0530

    Added: iterators and variable scope.

---
+++ Module-10/iterator.py
@@ -0,0 +1,19 @@
+import random
+
+
+class Dice:
+    def __init__(self, rolls):
+        self.rolls = rolls
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count < self.rolls:
+            self.count += 1
+            return random.randint(1, 6)
+        else:
+            raise StopIteration
+dice = [d for d in Dice(1)]
+print(dice)
---
+++ Module-10/var-scope.py
@@ -0,0 +1,7 @@
+
+name = "Aakhya"
+def display_name():
+    name = "Shashwat"
+    print(name)
+
+display_name()
